ba666c6dad53abd91dc49a0ffaf49c54e4336333
Damián Ponce <dami.ponce8@gmail.com>
2023-09-03 20:28:08 -0300

    updated descriptions

--- components/about.tsx
+++ components/about.tsx
@@ -15,23 +15,25 @@ const Link = ({ url, children }: { url: string; children: string }) => (
 
 const About = ({ lang }: Props) => {
   return {
-    en: (
+    es: (
       <p className='mb-4'>
-        I’ve always been interested in programming as my main way to be
-        creative, solving problems one way or another. I also enjoy graphic
-        design from time to time, be it for random poster design or even for
-        good UX. <br />
+        Siempre estuve interesado en la programación como mi principal manera de
+        ser creativo, arreglando problemas de alguna manera. También disfruto el
+        diseño gráfico casualmente, ya sea para un diseño de un poster random o
+        hasta buena UX.
         <br />
-        Another interest of mine is electronics, probably from having gone to a
-        “technical” high-school. I also like tinkering with electronics stuff
-        ocasionally. When I’m not working on my projects, I enjoy gaming, music,
-        and even programming for fun.
         <br />
-        <br />I am excited to see where my career takes me in the future, and I
-        am always looking for new opportunities to learn and grow.
+        Otro interes que tengo es la electrónica, probablemente por haber
+        estudiado en una escuela secundaria “técnica”. También me gusta meterme
+        en proyectos de electrónica a veces. Cuando no estoy con mis proyectos,
+        disfruto algunos videojuegos, escuchar música y programar por hobby.
+        <br />
+        <br />
+        Estoy entusiasmado de ver a donde me lleva mi carrera, y siempre estoy
+        en búsqueda de oportunidades para aprender y crecer.
       </p>
     ),
-    es: (
+    en: (
       <p className='mb-4'>
         I’ve always been interested in programming as my main way to be
         creative, solving problems one way or another. I also enjoy graphic
--- components/footer.tsx
+++ components/footer.tsx
@@ -18,28 +18,28 @@ const Footer = ({ lang }: Props) => {
     es: (
       <footer className='max-w-md pb-16 text-sm text-neutral-500 sm:pb-0'>
         <p>
-          Ligeramente designed in{' '}
-          <Link url='https://www.figma.com/'>Figma</Link> and coded in{' '}
-          <Link url='https://code.visualstudio.com/'>Visual Studio Code</Link>{' '}
-          by yours truly. Built with{' '}
-          <Link url='https://nextjs.org/'>Next.js</Link> and{' '}
-          <Link url='https://tailwindcss.com/'>Tailwind CSS</Link>, deployed
-          with <Link url='https://vercel.com/'>Vercel</Link>. All text is set in
-          the <Link url='https://rsms.me/inter/'>Inter</Link> typeface.
+          Diseñado por{' '}
+          <Link url='https://brittanychiang.com/'>Brittany Chiang</Link> y
+          programador por mí en{' '}
+          <Link url='https://code.visualstudio.com/'>Visual Studio Code</Link>.
+          Hecho con <Link url='https://nextjs.org/'>Next.js</Link> y{' '}
+          <Link url='https://tailwindcss.com/'>Tailwind CSS</Link>,{' '}
+          <Link url='https://vercel.com/'>Vercel</Link>. All text is set in the{' '}
+          <Link url='https://rsms.me/inter/'>Inter</Link> typeface.
         </p>
       </footer>
     ),
     en: (
       <footer className='max-w-md pb-16 text-sm text-neutral-500 sm:pb-0'>
         <p>
-          Loosely designed in <Link url='https://www.figma.com/'>Figma</Link>{' '}
-          and coded in{' '}
+          Designed by{' '}
+          <Link url='https://brittanychiang.com/'>Brittany Chiang</Link> and
+          coded in{' '}
           <Link url='https://code.visualstudio.com/'>Visual Studio Code</Link>{' '}
-          by yours truly. Built with{' '}
-          <Link url='https://nextjs.org/'>Next.js</Link> and{' '}
-          <Link url='https://tailwindcss.com/'>Tailwind CSS</Link>, deployed
-          with <Link url='https://vercel.com/'>Vercel</Link>. All text is set in
-          the <Link url='https://rsms.me/inter/'>Inter</Link> typeface.
+          by me. Built with <Link url='https://nextjs.org/'>Next.js</Link> and{' '}
+          <Link url='https://tailwindcss.com/'>Tailwind CSS</Link>, desplegado
+          en <Link url='https://vercel.com/'>Vercel</Link>. Todos los textos
+          utilizan la fuente <Link url='https://rsms.me/inter/'>Inter</Link>.
         </p>
       </footer>
     ),

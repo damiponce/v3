bec9a4bdad5c4215f3fc5126044d85b458d01d72
Damián Ponce <dami.ponce8@gmail.com>
2023-11-03 18:43:50 -0300

    added GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import '../styles/index.css';
 import '../components/github-markdown.css';
 import { appWithTranslation } from 'next-i18next';
 import { Inter, Source_Code_Pro } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 const sourceCodePro = Source_Code_Pro({ subsets: ['latin'] });
@@ -20,6 +21,13 @@ function Root({ Component, pageProps }: AppProps) {
         `}</style>
         <meta name='robots' content='all' />
         <meta name='googlebot' />
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WCJSFKXG');`}
+        </Script>
         <Component {...pageProps} />
       </div>
     </StrictMode>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCJSFKXG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </Html>
   );

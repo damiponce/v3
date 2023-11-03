419c7b0dda64b73dbc8c3ec2573298b39a1abf73
Damián Ponce <dami.ponce8@gmail.com>
2023-11-03 19:00:23 -0300

    removed GTM and added tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { useTranslation } from 'next-i18next';
+import Script from 'next/script';
 
 export default function Document() {
   const { i18n } = useTranslation();
@@ -8,15 +9,19 @@ export default function Document() {
       <Head>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-W2WTZGZP5B' />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-W2WTZGZP5B');
+        `}
+      </Script>
       <body>
         <Main />
         <NextScript />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCJSFKXG"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
       </body>
     </Html>
   );

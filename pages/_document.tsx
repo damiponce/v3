fbf7d377c5d914106488ed510f781e5c5d77d1b6
Damián Ponce <dami.ponce8@gmail.com>
2024-02-15 23:45:37 -0300

    OG/TW images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,14 @@ export default function Document() {
     <Html lang={i18n.language}>
       <Head>
         <link rel='icon' href='/favicon.ico' />
+        <meta property="og:image" content="<generated>" />
+<meta property="og:image:type" content="<generated>" />
+<meta property="og:image:width" content="<generated>" />
+<meta property="og:image:height" content="<generated>" />
+<meta property="twitter" content="<generated>" />
+<meta property="twitter:type" content="<generated>" />
+<meta property="twitter:width" content="<generated>" />
+<meta property="twitter:height" content="<generated>" />
       </Head>
       <body>
         <Main />

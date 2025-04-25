6f1705a6f17d8bb602893818e28a5c256dd89337
Damián Ponce <dami.ponce8@gmail.com>
2025-04-25 00:41:18 -0300

    posthog + vercel fix try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,8 +24,11 @@ function Root({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      // api_host:
+      //   process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+
+      api_host: 'https://www.your-domain.com/ingest',
+      ui_host: 'https://us.posthog.com',
       // Enable debug mode in development
       loaded: (posthog) => {
         if (process.env.NODE_ENV === 'development') posthog.debug();
